1634abf74f06517a97af0254e72fd3e06d893637
Bob Pham <bobbaopham@gmail.com>
2023-11-24 20:55:35 -0800

    Joined subtables

--- src/backend/ViewTables.php
+++ src/backend/ViewTables.php
@@ -4,36 +4,24 @@
     <input type="hidden" id="viewTables" name="viewTables">
     <select name="tableToView" id="tableToView">
       <option value="None">N/A</option>
-      <option value="Team">Team</option>
-      <option value="MascotColour">MascotColour</option>
-      <option value="Player">Player</option>
-      <option value="PlayerXPLevel">PlayerXPLevel</option>
-      <option value="Item">Item</option>
-      <option value="ItemEffectType">ItemEffectType</option>
-      <option value="ItemTypeUses">ItemTypeUses</option>
-      <option value="Mission">Mission</option>
-      <option value="MissionEventNameXP">MissionEventNameXP</option>
-      <option value="Location">Location</option>
-      <option value="BiomeAttackBonus">BiomeAttackBonus</option>
-      <option value="Gym">Gym</option>
-      <option value="Pokestop">Pokestop</option>
-      <option value="Egg">Egg</option>
-      <option value="EggSpecies">EggSpecies</option>
-      <option value="Pokemon">Pokemon</option>
-      <option value="PokemonSpeciesCP">PokemonSpeciesCP</option>
-      <option value="PokemonSpeciesTypes">PokemonSpeciesTypes</option>
-      <option value="NPC">NPC</option>
-      <option value="RoleCanBattle">RoleCanBattle</option>
+      <option value="Team-MascotColour">Team</option>
+      <option value="Player-PlayerXPLevel">Player</option>
       <option value="PlayerOwnsItem">PlayerOwnsItem</option>
       <option value="PlayerCompletedMission">PlayerCompletedMission</option>
-      <option value="Battle">Battle</option>
-      <option value="LeagueMaxCP">LeagueMaxCP</option>
       <option value="PlayerCapturedPokemon">PlayerCapturedPokemon</option>
       <option value="PlayerCapturedEgg">PlayerCapturedEgg</option>
       <option value="PlayerVisitedPokestop">PlayerVisitedPokestop</option>
+      <option value="Item-ItemEffectType-ItemTypeUses">Item</option>
+      <option value="Mission-MissionEventNameXP">Mission</option>
+      <option value="Location-BiomeAttackBonus">Location</option>
+      <option value="Gym-BiomeAttackBonus">Gym</option>
+      <option value="Pokestop-BiomeAttackBonus">Pokestop</option>
+      <option value="Egg-EggSpecies">Egg</option>
+      <option value="Pokemon-PokemonSpeciesCP-PokemonSpeciesTypes">Pokemon</option>
+      <option value="NPC-RoleCanBattle">NPC</option>
+      <option value="Battle-LeagueMaxCP">Battle</option>
       <option value="NPCAppearedAtPokestop">NPCAppearedAtPokestop</option>
-      <option value="NPCSighting">NPCSighting</option>
-      <option value="NPCSightingEventName">NPCSightingEventName</option>
+      <option value="NPCSighting-NPCSightingEventName">NPCSighting</option>
     </select>
     <input type="submit" value="Submit">
   </form>
@@ -43,17 +31,24 @@ include_once("./util.php");
 function viewTables() {
   // These are for basic queries
   global $db_conn;
-  $table = $_GET["tableToView"];
+  $value = explode('-', $_GET["tableToView"]);
+  $table = $value[0];
+
   if ($table === "None") {
     return;
   }
+
   $result = executePlainSQL("SELECT Count(*) FROM $table");
   if (($row = oci_fetch_row($result)) != false) {
     echo '<div class="subheader">';
     echo "<p> We Found " . $row[0] . " Results for $table!</p>";
     echo '</div>';
   }
-  $result = executePlainSQL("SELECT * FROM " . $table);
+  
+  $to_select = implode(" NATURAL JOIN ", $value);
+  $query = "SELECT * FROM " . $to_select;
+
+  $result = executePlainSQL($query);
   printResult($result);
 }
 
